9e795100413bf220df06ca9de0a69cafad27b342
therion-666 <strike201420141@gmail.com>
2022-05-14 14:34:33 +0300

    fixed indentation

--- rollup.config.js
+++ rollup.config.js
@@ -8,54 +8,54 @@ import css from "rollup-plugin-css-only"
 const production = !process.env.ROLLUP_WATCH
 
 function serve() {
-	let server
+  let server
 
-	function toExit() {
-		if (server) server.kill(0)
-	}
+  function toExit() {
+    if (server) server.kill(0)
+  }
 
-	return {
-		writeBundle() {
-			if (server) return
-			server = require("child_process").spawn("npm", ["run", "start", "--", "--dev"], {
-				stdio: ["ignore", "inherit", "inherit"],
-				shell: true
-			})
+  return {
+    writeBundle() {
+      if (server) return
+      server = require("child_process").spawn("npm", ["run", "start", "--", "--dev"], {
+        stdio: ["ignore", "inherit", "inherit"],
+        shell: true
+      })
 
-			process.on("SIGTERM", toExit)
-			process.on("exit", toExit)
-		}
-	}
+      process.on("SIGTERM", toExit)
+      process.on("exit", toExit)
+    }
+  }
 }
 
 export default {
-	input: "src/main.js",
-	output: {
-		sourcemap: true,
-		format: "iife",
-		name: "app",
-		file: "public/build/bundle.js"
-	},
-	plugins: [
-		svelte({
-			compilerOptions: {
-				dev: !production
-			}
-		}),
+  input: "src/main.js",
+  output: {
+    sourcemap: true,
+    format: "iife",
+    name: "app",
+    file: "public/build/bundle.js"
+  },
+  plugins: [
+    svelte({
+      compilerOptions: {
+        dev: !production
+      }
+    }),
 
-		css({ output: "bundle.css" }),
+    css({ output: "bundle.css" }),
 
-		resolve({
-			browser: true,
-			dedupe: ["svelte"]
-		}),
-		commonjs(),
+    resolve({
+      browser: true,
+      dedupe: ["svelte"]
+    }),
+    commonjs(),
 
-		!production && serve(),
-		!production && livereload("public"),
-		production && terser()
-	],
-	watch: {
-		clearScreen: false
-	}
+    !production && serve(),
+    !production && livereload("public"),
+    production && terser()
+  ],
+  watch: {
+    clearScreen: false
+  }
 }
